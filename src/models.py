23ffc8f75785b5e6596136dd126ab96960653887
Daniela Bryon <daniellabryren@icloud.com>
2024-12-05 19:43:18 +0000

    Star Wars REST API finished

--- src/models.py
+++ src/models.py
@@ -3,17 +3,120 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites": [favorite.serialize() for favorite in self.favorites]
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    climate = db.Column(db.String(120))
+    population = db.Column(db.Integer)
+    terrain = db.Column(db.String(120))
+    diameter = db.Column(db.String(50))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "terrain": self.terrain,
+            "diameter": self.diameter
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    gender = db.Column(db.String(20))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+    birth_year = db.Column(db.String(20))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year
+        }
+
+class Film(db.Model):
+    __tablename__ = 'film'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    episode_id = db.Column(db.Integer, nullable=False)
+    director = db.Column(db.String(80))
+    producer = db.Column(db.String(120))
+    release_date = db.Column(db.String(20))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "episode_id": self.episode_id,
+            "director": self.director,
+            "producer": self.producer,
+            "release_date": self.release_date
+        }
+
+class Starship(db.Model):
+    __tablename__ = 'starship'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    model = db.Column(db.String(120))
+    manufacturer = db.Column(db.String(120))
+    cost_in_credits = db.Column(db.String(50))
+    length = db.Column(db.String(50))
+    crew = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+    cargo_capacity = db.Column(db.String(50))
+    starship_class = db.Column(db.String(120))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "starship_class": self.starship_class
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    resource_type = db.Column(db.String(50), nullable=False)
+    resource_id = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "resource_type": self.resource_type,
+            "resource_id": self.resource_id
         }
